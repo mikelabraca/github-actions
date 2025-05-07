ce6f2a31b4b4f09e0f13797542e1e3e630426d69
Miguel Angel Labraca <mlabraca@bodas.net>
2025-05-08 01:59:41 +0200

    one more echo

--- packages/codeowners-labeler/action.sh
+++ packages/codeowners-labeler/action.sh
@@ -43,7 +43,7 @@ if [ -f "$CODEOWNERS_PATH" ]; then
         # Add the labels for the matching pattern
         for owner in $owners; do
           echo "Adding owner=$owner for file: $file"
-          gh pr label $PR_NUMBER --add "$owner" --repo "$REPO" --token "$GITHUB_TOKEN"
+          gh pr edit $PR_NUMBER --add-label "$owner"
         done
       fi
     done
